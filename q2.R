7eb43c7088269ec798f10408226a89eaa8396492
Benjamin Cox <ben.10.cox@hotmail.co.uk>
2019-11-06 23:10:40 +0000

    lm comment

--- q2.R
+++ q2.R
@@ -8,7 +8,7 @@ load("databp.Rdata")
 #a
 databp <- subset(databp, select = -c(R))
 
-databp_cc <- databp[complete.cases(databp), ]
+databp_cc <- databp[complete.cases(databp),]
 
 cor(databp_cc, method = "pearson")
 
@@ -31,18 +31,25 @@ databp_sri$recovtime <-
 cor(databp_sri, method = "pearson")
 
 #e
-databp_pmm <- complete(mice(data = databp, method = "pmm", m = 1, donors = 1))
+databp_pmm <-
+  complete(mice(
+    data = databp,
+    method = "pmm",
+    m = 1,
+    donors = 1
+  ))
 
 cor(databp_pmm, method = "pearson")
 
 ##########
 
-cc_lm <- lm(data = databp_cc, recovtime ~ .)
+cc_lm <-
+  lm(data = databp_cc, recovtime ~ .) #standard behaviour in R is for lm to drop nas, this just makes it explicit
 
 na_ind <- which(is.na(databp$recovtime))
 
 #complete case analysis
-databp_cc <- databp[complete.cases(databp), ]
+databp_cc <- databp[complete.cases(databp),]
 
 
 #mean imputation
@@ -53,19 +60,20 @@ databp_mi_sr$recovtime[na_ind] <-
 #regression imputation
 databp_ri_sr <- databp
 databp_ri_sr$recovtime[na_ind] <-
-  predict(cc_lm, databp_ri_sr[na_ind, ])
+  predict(cc_lm, databp_ri_sr[na_ind,])
 
 #stochastic regression imputation
 databp_sri_sr <- databp
 databp_sri_sr$recovtime[na_ind] <-
-  predict(cc_lm, databp_sri_sr[na_ind, ]) + rnorm(n = length(na_ind),
-                                                  mean = 0,
-                                                  sd = sigma(cc_lm))
-for (i in 1:length(na_ind)){
-  while (databp_sri_sr$recovtime[na_ind[i]] < 0){
-    databp_sri_sr$recovtime[na_ind[i]] <- predict(cc_lm, databp_sri_sr[na_ind[i], ]) + rnorm(n = 1,
-                                                                                          mean = 0,
-                                                                                          sd = sigma(cc_lm))
+  predict(cc_lm, databp_sri_sr[na_ind,]) + rnorm(n = length(na_ind),
+                                                 mean = 0,
+                                                 sd = sigma(cc_lm))
+for (i in 1:length(na_ind)) {
+  while (databp_sri_sr$recovtime[na_ind[i]] < 0) {
+    databp_sri_sr$recovtime[na_ind[i]] <-
+      predict(cc_lm, databp_sri_sr[na_ind[i],]) + rnorm(n = 1,
+                                                        mean = 0,
+                                                        sd = sigma(cc_lm))
   }
 }
 
@@ -80,13 +88,15 @@ databp_pmm_srnl <- databp
 
 sq_diff <- vector("list", length(na_ind))
 to_select <- double(length(na_ind))
-for (i in 1:length(sq_diff)){
-  sq_diff[i] <- list((cc_lm$fitted.values - predict(cc_lm, databp[na_ind[i], ]))^2)
+for (i in 1:length(sq_diff)) {
+  sq_diff[i] <-
+    list((cc_lm$fitted.values - predict(cc_lm, databp[na_ind[i],])) ^ 2)
   to_select[i] <- which.min(sq_diff[[i]])
 }
 
-for (i in 1:length(na_ind)){
-  databp_pmm_sr$recovtime[na_ind[i]] <- databp_cc$recovtime[which.min(sq_diff[[i]])]
+for (i in 1:length(na_ind)) {
+  databp_pmm_sr$recovtime[na_ind[i]] <-
+    databp_cc$recovtime[which.min(sq_diff[[i]])]
 }
 
 databp_pmm_srnl$recovtime[na_ind] <- databp_cc$recovtime[to_select]
@@ -97,9 +107,15 @@ cor(databp_mi_sr, method = "pearson")
 cor(databp_ri_sr, method = "pearson")
 cor(databp_sri_sr, method = "pearson")
 cor(databp_pmm_sr, method = "pearson")
-mean(databp_cc$recovtime); mean(databp_mi_sr$recovtime); mean(databp_ri_sr$recovtime); mean(databp_sri_sr$recovtime); mean(databp_pmm_sr$recovtime)
-sd(databp_cc$recovtime)/sqrt(sum(!is.na(databp_cc$recovtime)))
-sd(databp_mi_sr$recovtime)/sqrt(sum(!is.na(databp_mi_sr$recovtime)))
-sd(databp_ri_sr$recovtime)/sqrt(sum(!is.na(databp_ri_sr$recovtime)))
-sd(databp_sri_sr$recovtime)/sqrt(sum(!is.na(databp_sri_sr$recovtime)))
-sd(databp_pmm_sr$recovtime)/sqrt(sum(!is.na(databp_pmm_sr$recovtime)))
+mean(databp_cc$recovtime)
+mean(databp_mi_sr$recovtime)
+mean(databp_ri_sr$recovtime)
+mean(databp_sri_sr$recovtime)
+mean(databp_pmm_sr$recovtime)
+mean(databp_pmm_srnl$recovtime)
+sd(databp_cc$recovtime) / sqrt(sum(!is.na(databp_cc$recovtime)))
+sd(databp_mi_sr$recovtime) / sqrt(sum(!is.na(databp_mi_sr$recovtime)))
+sd(databp_ri_sr$recovtime) / sqrt(sum(!is.na(databp_ri_sr$recovtime)))
+sd(databp_sri_sr$recovtime) / sqrt(sum(!is.na(databp_sri_sr$recovtime)))
+sd(databp_pmm_sr$recovtime) / sqrt(sum(!is.na(databp_pmm_sr$recovtime)))
+sd(databp_pmm_srnl$recovtime) / sqrt(sum(!is.na(databp_pmm_srnl$recovtime)))
